c106fced36171f3cc8ce19c304fba206c9d852d7
salvatoremilone <salvatore.milone@outlook.it>
2023-07-29 18:01:17 +0200

    Initial Commit

---
+++ decus/decusApp.swift
@@ -0,0 +1,17 @@
+//
+//  decusApp.swift
+//  decus
+//
+//  Created by Salvatore Milone on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct decusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
